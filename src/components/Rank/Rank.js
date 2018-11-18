edb3d4a9413e50dbabb9fcc9247b0cf80977ec87
hristokrastev <swifty1009>
2018-11-18 13:57:14 +0200

    adding new project

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+	return (
+		<div className='ma4 mt0'>
+			<div className='white f3'>
+				{'Hristo, your current rank is...'}
+			</div>
+			<div className='white f1'>
+				{'#3'}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
