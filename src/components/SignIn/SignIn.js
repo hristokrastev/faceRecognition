1cfd4af495bbfe35c717d442f7e33e662d46acdb
hristokrastev <swifty1009>
2018-12-16 13:20:26 +0200

    fixing routes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -21,7 +21,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3001/signin', {
+		fetch('https://secure-beach-88293.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
