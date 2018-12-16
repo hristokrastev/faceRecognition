589e1236d368b7e04d12fbd0ae9d337996721841
hristokrastev <swifty1009>
2018-12-16 11:56:58 +0200

    preparing for dep.

---
+++ node/controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+	apiKey: 'e7b21376256e44b999f0cea9d4b35896'
+    });
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data=> {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to detect face with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries=>{
+		res.json(entries[0]);
+	})
+	.catch(err=> res.status(400).json('Unable to get entries'))
+}
+module.exports = {
+	handleImage,
+	handleApiCall
+};
---
+++ node/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'swifty1009',
+    database : 'smart_brain'
+  }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res)=>{ res.send(database.users) } );
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) } );
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) } );
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) } );
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) } );
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) } );
+
+
+app.listen(3001, ()=> {
+	console.log('app is running on port 3001');
+});
